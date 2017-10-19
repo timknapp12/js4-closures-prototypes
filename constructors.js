a4c5df41c4ca093cbd7014087f7236e60bd9920b
timknapp12 <timknapp12@gmail.com>
2017-10-19 15:34:29 -0600

    last commit

--- constructors.js
+++ constructors.js
@@ -8,7 +8,10 @@
 
 // Code here
 
-
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
 
 
@@ -26,7 +29,7 @@ function Employee(name, email, hireDate) {
 }
 
 // Code here
-
+let bob =  new Employee('Bob', 'bob@gmail.com', '01-02-98');
 
 
 
@@ -41,8 +44,17 @@ function Employee(name, email, hireDate) {
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
 // Code here
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+}
 
-
+this.Car.prototype.moveCar = function() {
+  this.move += 10;
+  return this.move;
+}
 
 
   //Function Invocations Here
@@ -51,7 +63,7 @@ function Employee(name, email, hireDate) {
   var mustang = new Car('Ford', 'Mustang', 2013);
   
   //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
-  
+
   prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
   mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
